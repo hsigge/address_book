68cf731f005d405ab0928dfc6dff87daa35c1c46
rhon <rhoneil.gabriel@me.com>
2018-09-04 16:19:00 +0200

    event handling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log("Hello August 2018 Cohort")
+document.addEventListener('DOMContentLoaded', () => {
+
+    const addContactForm = document.querySelector('.new-contact-form')
+        
+  
+    addContactForm.addEventListener('submit', event => {
+      event.preventDefault()
+  
+      const {
+        name,
+        email,
+        phone,
+        company,
+        notes,
+        twitter,
+      } = addContactForm.elements
+  
+      const contact = {
+        id: Date.now(),
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        company: company.value,
+        notes: notes.value,
+        twitter: twitter.value,
+      }
+  
+      console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+    })
+  })
