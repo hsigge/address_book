c4a7727c65bb67b6381ada6c63803a3687524e15
rhon <rhoneil.gabriel@me.com>
2018-09-06 14:16:23 +0200

    style html with tailwind css

--- src/app.js
+++ src/app.js
@@ -12,29 +12,29 @@ const renderContacts = () => {
     const ul = document.createElement('ul')
     
     contacts.forEach(contact => {
-      let li = document.createElement('li')
-      li.innerHTML = `
-    <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
-    <div class="card sm:flex sm:items-center px-6 py-4">
-      <img class="image block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="img/Portrait_Placeholder.png" alt="Portrait_Placeholder">
+      let card = document.createElement('card')
+      card.innerHTML = `
+    <div class="bg-grey-lighter mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+    <div class="card sm:flex sm:items-center px-6 py-4 text-red-dark font-thin hover:bg-grey">
+      <img class="image block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="Portrait.png" alt="Portrait_Placeholder">
       <div class="text-center sm:text-left sm:flex-grow">
         <div class="content mb-4">
-          <p class="text-xl leading-tight">${ contact.name }</p>
-          <p class="text-sm leading-tight text-grey-dark">${ contact.company }<br>
+          <p class="text-xl text-red-dark font-thin leading-tight">${ contact.name }</p>
+          <p class="text-sm text-red-dark font-thin leading-tight text-grey-dark">${ contact.company }<br>
           ${ contact.notes }<br>
           ${ contact.phone } | ${ contact.email } | 
           <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
           </p>
         </div>
         <div>
-          <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-grey text-grey hover:bg-grey hover:text-white"><a href="mailto:webmaster@example.com">Message</a></button>
+          <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-grey text-grey hover:bg-grey hover:text-white"><a href="mailto:webmaster@example.com">Email</a></button>
         </div>
       </div>
     </div>
   </div>
   &nbsp;
    `
-   ul.appendChild(li)
+   ul.appendChild(card)
     })
     div.appendChild(ul)
   } else {
